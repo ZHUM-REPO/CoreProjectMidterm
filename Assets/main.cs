533c537a1ff934991d8ae8cd210a165c4b6085bf
ranad-aziz <reenad.alzaidi2@gmail.com>
2026-05-21 16:10:51 +0300

    test

---
+++ Assets/main.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class main : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
